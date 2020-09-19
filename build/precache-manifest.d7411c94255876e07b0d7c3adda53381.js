e2137b8f4621271537dab7e3fd16d36c85b8b9d8
diegolarraz <diegolarraz1@gmail.com>
2020-09-19 09:59:28 +0100

    done

---
+++ build/precache-manifest.d7411c94255876e07b0d7c3adda53381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511b3a98b5e1515b56574657e88bc19a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c743ed78303fb1c91519",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "ec8fe6a3c05844def071",
+    "url": "/static/js/2.62d9b72b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.62d9b72b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c743ed78303fb1c91519",
+    "url": "/static/js/main.5ddbdabe.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
